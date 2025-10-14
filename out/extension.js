0e88ee9d9d79c578857276ed35ddaa9061ff6b37
Stéphane Bouget <stephane.bouget@orange.com>
2025-10-14 17:35:58 +0200

    Refactor debugger command implementation to toggle 'debugger;' insertion and removal in active line

--- out/extension.js
+++ out/extension.js
@@ -1,35 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+exports.activate = activate;
+exports.deactivate = deactivate;
 const vscode = require("vscode");
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-debugger" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('vscodedebugger.debugger', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        // vscode.window.showInformationMessage('debugger World!');
+    let disposable = vscode.commands.registerCommand("vscodedebugger.debugger", () => {
         const editor = vscode.window.activeTextEditor;
         if (!editor) {
             return;
         }
         const selection = editor.selection;
         const lineOfSelectedVar = selection.active.line;
-        editor.edit(editBuilder => {
-            editBuilder.insert(new vscode.Position(lineOfSelectedVar, 0), 'debugger');
+        const document = editor.document;
+        const lineText = document.lineAt(lineOfSelectedVar).text;
+        editor.edit((editBuilder) => {
+            if (lineText.includes("debugger")) {
+                // Remove all instances of 'debugger' in the line
+                const newText = lineText.replace(/\bdebugger;?\s?/g, "");
+                const range = document.lineAt(lineOfSelectedVar).range;
+                editBuilder.replace(range, newText);
+            }
+            else {
+                // Insert 'debugger;' at the beginning of the line
+                editBuilder.insert(new vscode.Position(lineOfSelectedVar, 0), "debugger; ");
+            }
         });
     });
     context.subscriptions.push(disposable);
 }
-exports.activate = activate;
-// this method is called when your extension is deactivated
 function deactivate() { }
-exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
